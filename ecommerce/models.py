8511d4e42157bb7fb80214bc6c19e55aded07683
mkskh <maksym.kharchenko@dci-student.org>
2024-03-12 20:52:16 +0100

    models initialized

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
+
