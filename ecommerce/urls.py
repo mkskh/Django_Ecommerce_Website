d2557989f13113be5ffb4dcaea1ceb0b513c2040
mkskh <maksym.kharchenko@dci-student.org>
2024-03-14 02:02:10 +0100

    added: About page; Links to ever product, detail page for every product

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'ecommerce'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('product_detail/<int:id>/', views.product_detail, name='product_detail'),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -7,3 +7,12 @@ def home(request):
     return render(request, 'ecommerce/home.html', {'products': products})
 
 
+def about(request):
+    return render(request, 'ecommerce/about.html')
+
+
+def product_detail(request, id):
+    product = models.Product.objects.get(pk=id)
+    return render(request, 'ecommerce/product_detail.html', {'product': product})
+
+
