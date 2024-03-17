ba9f04dc68d3928e71eb1654d1f9b0ed885f47df
mkskh <maksym.kharchenko@dci-student.org>
2024-03-17 22:50:00 +0100

    migrate cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.CharField(max_length=255)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.product} x {self.quantity}"
